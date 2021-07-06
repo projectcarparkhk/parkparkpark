6023ca39a9cc01589a7442b44366ef3f52742ab1
Anthony Siu <anthony.siu@shoplineapp.com>
2021-07-06 12:29:58 +0800

    feat: added frontpage

--- components/header.js
+++ components/header.js
@@ -1,14 +1,45 @@
 import Link from 'next/link'
+import { AppBar, Container } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    [theme.breakpoints.down('sm')]: {
+      display: 'flex',
+      justifyContent: 'center',
+    },
+  },
+  headerTitle: {
+    fontWeight: 'bold',
+    margin: '0.5rem 0 0.5rem 0',
+    [theme.breakpoints.down('sm')]: {
+      textAlign: 'center',
+    },
+  },
+  slogan: {
+    marginBottom: '0.5rem',
+    fontSize: '0.8rem',
+    [theme.breakpoints.down('sm')]: {
+      textAlign: 'center',
+    },
+  },
+}))
 export default function Header() {
+  const classes = useStyles()
   return (
     <>
-      <h1 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mt-8">
-        <Link href="/">
-          <span className="hover:underline">ParkParkPark!</span>
-        </Link>
-      </h1>
-      <div className="mb-6">全港商場免費泊車及停車場優惠</div>
+      <AppBar>
+        <Container maxWidth="xl" fixed className={classes.container}>
+          <div>
+            <h2 className={classes.headerTitle}>
+              <Link href="/">
+                <span className="hover:underline">ParkParkPark!</span>
+              </Link>
+            </h2>
+            <div className={classes.slogan}>全港商場免費泊車及停車場優惠</div>
+          </div>
+        </Container>
+      </AppBar>
     </>
   )
 }
--- server.js
+++ server.js
@@ -21,8 +21,8 @@ app.prepare().then(() => {
     } else {
       handle(req, res, parsedUrl)
     }
-  }).listen(3000, (err) => {
+  }).listen(4000, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log('> Ready on http://localhost:4000')
   })
 })
