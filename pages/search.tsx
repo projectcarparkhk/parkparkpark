f7e304e3521b6b98f10a0b77bff7e289c2fcf914
Brian Leung <brianleung1017@gmail.com>
2021-07-19 22:58:47 +0800

    add hot tags

--- pages/search.tsx
+++ pages/search.tsx
@@ -2,22 +2,62 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import SearchInput from '../components/search/input'
 import { Suggestion } from '../components/search/type'
+import { TagResponse } from '../types'
 import Header from '../components/header'
+import { getHotTags } from '../lib/api'
+import Link from 'next/link'
+import Container from '@material-ui/core/Container'
+import Chip from '@material-ui/core/Chip'
+import { Theme } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
 
-function Search() {
+interface IProps {
+    hotTags: TagResponse[]
+  }
+
+
+const useStyles = makeStyles((theme: Theme) => ({
+    chip: {
+      margin: theme.spacing(0, 1, 1, 0),
+    },
+  }))
+
+function Search({ hotTags }: IProps) {
     const router = useRouter()
-
+    const classes = useStyles()
+    
     function onSuggestionClick(suggestion: Suggestion) {
         router.push(`/${suggestion.type}/${suggestion.slug}`, undefined , { shallow: true })
     }
     return (
-        <>
+        <Container>
             <Header />
             <SearchInput
                 onSuggestionClick={onSuggestionClick}
             />
-        </>
+            <h3>熱門</h3>
+            <div>
+            {hotTags
+        .map((tag) => (
+          <Link href={`/tags/${tag.slug}`}>
+            <Chip
+              className={classes.chip}
+              key={tag.name}
+              label={tag.name}
+            />
+          </Link>
+        ))}
+            </div>
+        </Container>
     )
 }
 
 export default Search
+
+export async function getStaticProps({ preview = false }) {
+  const hotTags = await getHotTags(preview)
+  return {
+    props: { hotTags, preview },
+    revalidate: 1,
+  }
+}
