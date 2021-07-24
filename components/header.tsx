2309aeb3c354d89f0e53c964d8e171f56f0117e6
Brian Leung <brianleung1017@gmail.com>
2021-07-24 17:50:54 +0800

    Squashed commit of the following:
    commit be99a15aaf5e345d20ca7d520814d3f0d9b34752
    Author: Brian Leung <brianleung1017@gmail.com>
    Date:   Sat Jul 24 17:50:19 2021 +0800
    
        add search icon to header
    
    commit 7c08d5b2a48526a87cf96b99928ff7fd68309f5f
    Author: Brian Leung <brianleung1017@gmail.com>
    Date:   Sat Jul 24 17:44:27 2021 +0800
    
        fix search bar style
    
    commit 5b19f964142e69f5b1e8ad2979e601ee67009e18
    Author: Brian Leung <brianleung1017@gmail.com>
    Date:   Sat Jul 24 17:34:30 2021 +0800
    
        hide hot tags if suggestions appear

--- components/header.tsx
+++ components/header.tsx
@@ -4,13 +4,17 @@ import { makeStyles } from '@material-ui/core/styles'
 import { Theme } from '@material-ui/core/styles'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
+import SearchIcon from '@material-ui/icons/Search'
 
 const useStyles = makeStyles((theme: Theme) => ({
   appBar: {
     height: theme.mixins.toolbar.minHeight
   },
   toolBar: {
-    minHeight: theme.mixins.toolbar.minHeight
+    minHeight: theme.mixins.toolbar.minHeight,
+  },
+  title: {
+    flexGrow: 1,
   },
   offset: theme.mixins.toolbar,
   menuButton: {
@@ -28,11 +32,16 @@ export default function Header() {
           <MenuIcon />
         </IconButton>
         </Link>
-          <h2>
+          <h2 className={classes.title}>
             <Link href="/">
               <span className="hover:underline">ParkParkPark!</span>
             </Link>
           </h2>
+        <Link href="/search">
+        <IconButton edge="end" color="inherit" aria-label="search">
+          <SearchIcon />
+        </IconButton>
+        </Link>
         </Toolbar>
       </AppBar>
       <div className={classes.offset} />
--- pages/search.tsx
+++ pages/search.tsx
@@ -12,44 +12,52 @@ import { Theme } from '@material-ui/core/styles'
 import { makeStyles } from '@material-ui/core/styles'
 
 interface IProps {
-    hotTags: TagResponse[]
-  }
+  hotTags: TagResponse[]
+}
 
 
 const useStyles = makeStyles((theme: Theme) => ({
-    chip: {
-      margin: theme.spacing(0, 1, 1, 0),
-    },
-  }))
+  searchWrapper: {
+    marginTop: theme.spacing(2),
+  },
+  chip: {
+    margin: theme.spacing(0, 1, 1, 0),
+  },
+}))
 
 function Search({ hotTags }: IProps) {
-    const router = useRouter()
-    const classes = useStyles()
-    
-    function onSuggestionClick(suggestion: Suggestion) {
-        router.push(`/${suggestion.type}/${suggestion.slug}`, undefined , { shallow: true })
-    }
-    return (
-        <Container>
-            <Header />
-            <SearchInput
-                onSuggestionClick={onSuggestionClick}
-            />
-            <h3>熱門</h3>
-            <div>
+  const router = useRouter()
+  const classes = useStyles()
+
+  function onSuggestionClick(suggestion: Suggestion) {
+    router.push(`/${suggestion.type}/${suggestion.slug}`, undefined, { shallow: true })
+  }
+  return (
+    <Container>
+      <Header />
+      <div className={classes.searchWrapper}>
+      <SearchInput
+        onSuggestionClick={onSuggestionClick}
+      >
+        <>
+          <h3>熱門</h3>
+          <div>
             {hotTags
-        .map((tag) => (
-          <Link href={`/tags/${tag.slug}`}>
-            <Chip
-              className={classes.chip}
-              key={tag.name}
-              label={tag.name}
-            />
-          </Link>
-        ))}
-            </div>
-        </Container>
-    )
+              .map((tag) => (
+                <Link key={tag.name} href={`/tags/${tag.slug}`}>
+                  <Chip
+                    className={classes.chip}
+                    key={tag.name}
+                    label={tag.name}
+                  />
+                </Link>
+              ))}
+          </div>
+        </>
+      </SearchInput>
+      </div>
+    </Container>
+  )
 }
 
 export default Search
